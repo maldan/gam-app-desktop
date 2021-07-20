51e8fb7edf1cda2d81fc67641c171dce5a77f474
Roman <blackwanted@yandex.ru>
2021-07-20 21:24:57 +0300

    Fix -- Arm64

--- internal/app/desktop/desktop.go
+++ internal/app/desktop/desktop.go
@@ -4,7 +4,10 @@ import (
 	"embed"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/maldan/go-restserver"
 	"github.com/zserge/lorca"
@@ -17,12 +20,29 @@ func Start(frontFs embed.FS) {
 	var port = flag.Int("port", 16001, "Server Port")
 	_ = flag.Int("clientPort", 8080, "Client Port")
 	var gui = flag.Bool("gui", false, "Use Gui")
+	var initDev = flag.Bool("initDev", false, "Install dev")
 	var width = flag.Int("width", 1100, "Window Width")
 	var height = flag.Int("height", 900, "Window Height")
-	var dataDir = flag.String("data-dir", "db", "Data Directory")
-	_ = flag.String("app-id", "id", "App id")
+	var dataDir = flag.String("dataDir", "db", "Data Directory")
+	_ = flag.String("appId", "id", "App id")
 	flag.Parse()
 	DataDir = *dataDir
+
+	// Copy as dev app
+	if *initDev {
+		dirname, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		GamAppDir := strings.ReplaceAll(dirname, "\\", "/") + "/.gam-app"
+		source, _ := os.Open(os.Args[0])
+		os.MkdirAll(GamAppDir+"/dev-desktop-v0.0.0", 0755)
+		destination, err := os.Create(GamAppDir + "/dev-desktop-v0.0.0/app.exe")
+		if err != nil {
+			panic(err)
+		}
+		io.Copy(destination, source)
+	}
 
 	if *gui {
 		go (func() {
